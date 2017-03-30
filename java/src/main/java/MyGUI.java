7945c54ab544715a692669f0b69220137a80b629
iGuest <tzchen@uw.edu>
2017-03-30 14:28:39 -0700

    java stuff

--- java/src/main/java/MyGUI.java
+++ java/src/main/java/MyGUI.java
@@ -6,24 +6,45 @@ import java.util.*;
  * A demo of GUI programming with Swing
  */
 public class MyGUI extends JFrame {
-	
 	public MyGUI() {
-		super("The Skywalker Family");
+		super("My GUI");
 		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		
 		JButton theButton = new JButton("Click me!");
-
-
-
-
 		
-		this.add(theButton);
+		theButton.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				System.out.println("clicked");
+  			}
+		});
+
+		JButton theButton2 = new JButton("No me!");
+		theButton2.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				System.out.println("HA!");
+			}
+		});
+
+
+		JPanel container = new JPanel();
+		container.setLayout(new BoxLayout(container, BoxLayout.Y_AXIS));
 		
+		container.add(theButton);
+		container.add(theButton2);
+		JPanel otherContainer = new JPanel();
+		otherContainer.setLayout(new BoxLayout(otherContainer, BoxLayout.X_AXIS));
+		otherContainer.add(new JButton("A"));
+		otherContainer.add(new JButton("B"));
+		otherContainer.add(new JButton("C"));
+
+		this.add(container);
+		container.add(otherContainer);
+
 		this.pack();
 		this.setVisible(true);		
 	}
-	
+
 	public static void main(String[] args) {
-    new MyGUI(); //create the frame
-  }
+    	new MyGUI(); //create the frame
+ 	}
 }
